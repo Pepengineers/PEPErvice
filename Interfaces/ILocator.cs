2926956c6817be1189acef6fb550786cc5063adb
Mikhanil <raeinforce@gmail.com>
2024-03-11 22:32:01 +0300

    remove debug tools

--- Interfaces/ILocator.cs
+++ Interfaces/ILocator.cs
@@ -4,7 +4,6 @@ namespace PEPEngineers.PEPErvice.Interfaces
 {
 	public interface ILocator
 	{
-		IReadOnlyCollection<object> Instances { get; }
 		TService Resolve<TService>() where TService : IService;
 		ILocator Resolve<TService>(out TService value) where TService : IService;
 	}
